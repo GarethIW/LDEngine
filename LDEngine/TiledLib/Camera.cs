3da6784c588d95f245912c8806d2f83434e06be6
Gareth Williams <gareth@garethw.net>
2014-04-05 13:07:01 +0100

    A few fixes found while writing the readme!

--- LDEngine/TiledLib/Camera.cs
+++ LDEngine/TiledLib/Camera.cs
@@ -29,12 +29,8 @@ namespace TiledLib
         float shakeAmount;
         Vector2 shakeOffset = Vector2.Zero;
 
-        /// <summary>
-        /// Initialise the camera, using the game map to define the boundaries
-        /// </summary>
-        /// <param name="vp">Graphics viewport</param>
-        /// <param name="map">Game Map</param>
-        public Camera(int width, int height, Map map)
+
+        public Camera(int width, int height, int boundswidth, int boundsheight)
         {
             Instance = this;
 
@@ -42,23 +38,19 @@ namespace TiledLib
             Width = width;
             Height = height;
 
-            ClampRect = new Rectangle((Width / 2), (Height / 2), (map.Width * map.TileWidth) - (Width / 2), (map.Height * map.TileHeight) - (Height / 2));
-
-            if (map.Properties.Contains("CameraBoundsLeft"))
-                ClampRect.X = Convert.ToInt32(map.Properties["CameraBoundsLeft"]) * map.TileWidth;
-            if (map.Properties.Contains("CameraBoundsTop"))
-                ClampRect.Y = Convert.ToInt32(map.Properties["CameraBoundsTop"]) * map.TileHeight;
-            if (map.Properties.Contains("CameraBoundsWidth"))
-                ClampRect.Width = Convert.ToInt32(map.Properties["CameraBoundsWidth"]) * map.TileWidth;
-            if (map.Properties.Contains("CameraBoundsHeight"))
-                ClampRect.Height = Convert.ToInt32(map.Properties["CameraBoundsHeight"]) * map.TileHeight;
+            ClampRect = new Rectangle((Width / 2), (Height / 2), (boundswidth) - (Width / 2), (boundsheight) - (Height / 2));
 
             // Set initial position and target
             Position.X = ClampRect.X;
             Position.Y = ClampRect.Y;
             Target = new Vector2(ClampRect.X, ClampRect.Y);
         }
-        
+
+        public Camera(int width, int height, Map map)
+            : this(width, height, map.Width * map.TileWidth, map.Height * map.TileHeight)
+        {
+        }
+
 
         /// <summary>
         /// Update the camera
@@ -83,11 +75,10 @@ namespace TiledLib
             // Move camera toward target
             Position = Vector2.Lerp(Position, Target, Speed);
 
-            //Rotation = TurnToFace(Vector2.Zero, AngleToVector(RotationTarget, 1f), Rotation, 0.02f);
-
             CameraMatrix = Matrix.CreateTranslation(-Position.X + shakeOffset.X, -Position.Y + shakeOffset.Y, 0) *
-                           Matrix.CreateScale(Zoom)*Matrix.CreateRotationZ(Rotation) *Matrix.CreateTranslation(Width / 2f, Height/2f, 0);
-            //CameraMatrix *= Matrix.CreateRotationZ(Rotation);
+                           Matrix.CreateScale(Zoom) * 
+                           Matrix.CreateRotationZ(Rotation) * 
+                           Matrix.CreateTranslation(Width / 2f, Height / 2f, 0);
         }
 
         public void Shake(double time, float amount)
