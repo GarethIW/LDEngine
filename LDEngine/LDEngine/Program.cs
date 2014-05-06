b9b0ed1cb25c228649283f795344c6294c6db503
Jesse Litton <monodevelop.git@eternaldusk.com>
2014-05-06 17:28:31 -0500

    Add Linux solution files.
    * map.tmx: Add a version of map.tmx file for Linux. This can be
      unified with the Windows file, after push of updated example
      game code.
    
    * LDEngine.Linux.csproj: and TiledLib.Linux.csproj: Add
      Linux-specific project (to allow for different references).
    
    * LDEngineContent.Linux.contentproj: Content project for Linux.
      This is required to build the spritefont. It is not necessary to
      compile the other PNG files, as MonoGame can use them
      directly - they are only included for completeness.
    
    * Silkscreen.ttf: Add font file for Linux.
      With the current Linux pipeline, the font must be in the
      same directory and named to match the tag in .spritefont.
    
    * blank.png, testhero.png, particles.png, testtiles.png:
      Add files for Linux project.  Linking to Windows content
      project files will not work, due to the incomplete
      content pipeline implementation in the current
      MonoGame.
    
    * font.spritefont: SpriteFont file for Linux.
    
    * .gitignore: Add ignore for MonoDevelop user preferences.
    
    * Program.cs: Removed conditionals that limited the program
      loop to Windows/360 only.

--- LDEngine/LDEngine/Program.cs
+++ LDEngine/LDEngine/Program.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace LDEngine
 {
-#if WINDOWS || XBOX
     static class Program
     {
         /// <summary>
@@ -16,6 +15,5 @@ namespace LDEngine
             }
         }
     }
-#endif
 }
 
